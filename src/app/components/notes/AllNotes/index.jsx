f4ef83ffb3531ec4373516b55056ca39f12d2c61
Louis Kenyon <louis.kenyon@ageas.co.uk>
2019-03-27 11:22:22 +0000

    added card component#

--- src/app/components/notes/AllNotes/index.jsx
+++ src/app/components/notes/AllNotes/index.jsx
@@ -1,36 +1,19 @@
 import React from 'react';
-import {Link} from 'react-router-dom';
+
+import Card from '../Card';
+
 import styles from '../../../../themes/style.scss';
 
 class AllNotes extends React.Component {
     renderAll() {
-        //Map array in reverse order so as to render the newest notes first.
-        //Shallow copy so as not to alter original array.
         return this.props.notes.slice(0).reverse().map((note) => {
             return (
-                <div className={`col-xs-1 col-md-4 col-xl-3 d-flex align-items-stretch ${styles.column}`} key={note.id}>
-                    <div className={`ui raised card ${styles.card}`}>
-                        <div className="content">
-                            <i
-                                onClick={() => this.props.deleteNote(note.id)}
-                                className={`right floated close icon ${styles["delete-button"]}`}
-                            />
-                            <div className={`header ${styles.header}`}>
-                                {note.title}
-                            </div>
-                            <div className={`description ${styles.body}`}>
-                                <p>{note.body}</p>
-                            </div>
-                        </div>
-                        <div className="extra content">
-                            <span className="right floated pencil alternate">
-                                <Link to={`/edit/${note.id}`}>
-                                    <i className={`pencil alternate icon ${styles["edit-button"]}`}/>
-                                </Link>
-                            </span>
-                        </div>
-                    </div>
-                </div>
+                <Card
+                    id={note.id}
+                    title={note.title}
+                    body={note.body}
+                    deleteFunc={this.props.deleteNote}
+                />
             )
         })
     }
---
+++ src/app/components/notes/Card/index.jsx
@@ -0,0 +1,39 @@
+import React from 'react';
+import {Link} from 'react-router-dom';
+
+import styles from '../../../../themes/style.scss';
+
+class Card extends React.Component {
+    render() {
+        return (
+            <div className={`col-xs-1 col-md-4 col-xl-3 d-flex align-items-stretch ${styles.column}`} key={this.props.id}>
+                <div className={`ui raised card ${styles.card}`}>
+                    <div className={`content`}>
+                        <i
+                            onClick={() => this.props.deleteFunc(this.props.id)}
+                            className={`right floated close icon ${styles["delete-button"]}`}
+                        />
+                        <div className={`header ${styles.header}`}>
+                            {this.props.title}
+                        </div>
+                        <div className={`description ${styles.body}`}>
+                            <p>
+                                {this.props.body}
+                            </p>
+                        </div>
+                    </div>
+                    <div className={"extra content"}>
+                        <span className="right floated pencil alternate">
+                            {/*pass props to link*/}
+                            <Link to={`/edit/${this.props.id}`}>
+                                <i className={`pencil alternate icon ${styles["edit-button"]}`}/>
+                            </Link>
+                        </span>
+                    </div>
+                </div>
+            </div>
+        )
+    }
+}
+
+export default Card;
