87cdae34d760c007492765a2b27b9bf857e220c9
Louis Kenyon <louis.kenyon@ageas.co.uk>
2019-04-17 16:18:31 +0100

    tested combineReducers

--- src/app/reducers/index.js
+++ src/app/reducers/index.js
@@ -1,7 +1,7 @@
 import {combineReducers} from 'redux';
 import {reducer as formReducer} from 'redux-form';
 import noteReducer from './note/note.reducer';
-import commonReducer from './common/'
+import commonReducer from './common/index'
 
 export default combineReducers({
     form: formReducer,
