bdbd106186ef49356ce4cd426597fe5cf2f01dc3
Louis Kenyon <louis.kenyon@ageas.co.uk>
2019-04-25 12:15:27 +0100

    Add additional path coverage to unit test for TextArea

--- src/__tests__/components/common/reduxFormFields/TextArea.test.js
+++ src/__tests__/components/common/reduxFormFields/TextArea.test.js
@@ -15,11 +15,34 @@ describe('TextArea', () => {
             input: {
                 foo: 'bar'
             }
-        }
+        };
+        jest.clearAllMocks()
     });
     describe('render', () => {
         it('should render textarea with placeholder and text in error state if touched and error = true', () => {
             expect(wrapper()).toMatchSnapshot();
+        })
+    });
+    describe('functionality', () => {
+        it('should have class of error when touched = true and error is truthy', () => {
+            expect(wrapper().find('div').first().hasClass('error')).toBe(true);
+        });
+        it('should not have a class of error when touch = false or error is false', () => {
+            props.meta = {
+                touched: false,
+                error: ''
+            };
+            expect(wrapper().find('div').first().hasClass('error')).toBe(false);
+            props.meta = {
+                touched: true,
+                error: ''
+            };
+            expect(wrapper().find('div').first().hasClass('error')).toBe(false);
+            props.meta = {
+                touched: false,
+                error: 'Error string'
+            };
+            expect(wrapper().find('div').first().hasClass('error')).toBe(false);
         })
     })
 });
